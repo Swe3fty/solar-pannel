dd5d96e2092dae2ed73d678dfeb610d784be3d9d
Colin503 <colinrousseau35@gmail.com>
2025-06-05 15:31:48 +0200

    upgrade de la carte

--- js/map.js
+++ js/map.js
@@ -1,3 +1,7 @@
+'use stric';
+
+
+
 var map = L.map('map').setView([48.390394, -4.486076],   13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -8,3 +12,29 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 L.marker([48.390394, -4.486076]).addTo(map)
     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
     .openPopup();
+
+
+// Charge les installations au chargement de la page
+requestInstallationsMap();
+
+// Récupère les miniatures depuis le serveur
+async function requestInstallationsMap() {
+    let response = await fetch('back/request.php/installationsMap'); // à faire
+
+    if (response.ok)
+        displayInstallationsMap(await response.json()) // Affiche les installations
+    else
+        displayErrors(response.status); // Affiche les erreurs
+}
+
+
+// Affiche toutes les installations sur la carte
+function displayInstallationsMap(installations) {
+    for (let i=0;i<installations.length;i++) {
+        L.marker([installations[i].latitude, installations[i].longitude]).addTo(map)
+            .bindPopup(installations[i].title);
+    }
+
+    // Ajoute un écouteur pour le clic sur les miniatures
+    //document.getElementById('thumbnails').addEventListener('click', requestPhoto);
+}
