f7a22f301da7999169b8a6fefaac6f91c9791c09
Colin503 <86229895+Colin503@users.noreply.github.com>
2025-06-06 11:09:50 +0200

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,4 +1,4 @@
-'use stric';
+'use strict';
 
 
 
@@ -17,16 +17,23 @@ L.marker([48.390394, -4.486076]).addTo(map)
 // Charge les installations au chargement de la page
 requestInstallationsMap();
 
-// Récupère les miniatures depuis le serveur
+// Récupère les installations depuis le serveur
 async function requestInstallationsMap() {
-    let response = await fetch('back/request.php/installationsMap'); // à faire
+    let response = await fetch('../back/request.php/?action=installationsMap&annee-installation=${encodeURIComponent(annee-installation)}&departementSelect=${encodeURIComponent(departementSelect)};');
 
     if (response.ok)
         displayInstallationsMap(await response.json()) // Affiche les installations
-    else
-        displayErrors(response.status); // Affiche les erreurs
 }
 
+
+async function requestInstallationMap(event) { 
+    let id = document.getElementById(event.target.id).getAttribute('installationid');
+    const response = await fetch('back/request.php/installationsMap/'+id);
+    
+    if (response.ok) {
+        //displayStats(await response.json()); // Affiche les détails de l'installation
+    }
+}
 
 // Affiche toutes les installations sur la carte
 function displayInstallationsMap(installations) {
@@ -36,5 +43,7 @@ function displayInstallationsMap(installations) {
     }
 
     // Ajoute un écouteur pour le clic sur les miniatures
-    //document.getElementById('thumbnails').addEventListener('click', requestPhoto);
+    L.addEventListener('click', requestInstallationMap);
 }
+
+
